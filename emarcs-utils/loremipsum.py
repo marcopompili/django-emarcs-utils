8fab960bb4d41a65e2c0b1779229c290fc0e1e57
Marco Pompili <marco.pompili@unipol.it>
2014-01-20 16:51:15 +0100

    Added models and loremipsum utility.

---
+++ emarcs-utils/loremipsum.py
@@ -0,0 +1,38 @@
+'''
+Created on 26/dic/2013
+
+@author: Marco Pompili
+'''
+
+def heading():
+    return 'heading'
+
+def long_paragraph():
+    return 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus'
+    
+def medium_paragraph():
+    return 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.'
+    
+def sentence():
+    return 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.'
+
+def city():
+    return 'Roma'
+
+def province():
+    return 'Roma'
+
+def road():
+    return 'via Garibaldi n.1'
+
+def zipcode():
+    return '00001'
+
+def site():
+    return 'http://www.mysite.com'
+    
+def email():
+    return 'myemail@provider.com'
+    
+def phone():
+    return '987/123456789'
---
+++ emarcs-utils/models.py
@@ -0,0 +1,19 @@
+'''
+Created on 20/gen/2014
+
+@author: Marco Pompili
+'''
+
+from django.db import models
+
+class UniqueBooleanField(models.BooleanField):
+    def pre_save(self, model_instance, add):
+        objects = model_instance.__class__.objects
+        # If True then set all others as False
+        if getattr(model_instance, self.attname):
+            objects.update(**{self.attname: False})
+        # If no true object exists that isnt saved model, save as True
+        elif not objects.exclude(id=model_instance.id)\
+                        .filter(**{self.attname: True}):
+            return True
+        return getattr(model_instance, self.attname)
